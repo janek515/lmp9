8de1fdfa6dc7352547d8835bae86ff9c7c0cd057
Jan Ochwat <janochwat@yahoo.com>
2024-12-20 01:44:11 +0100

    Implementacja algorytmu wyboru elementu głównego

--- src/gauss.c
+++ src/gauss.c
@@ -1,4 +1,5 @@
 #include "gauss.h"
+#include <math.h>
 
 /**
  * Zwraca 0 - elimnacja zakonczona sukcesem
@@ -7,13 +8,36 @@
 int eliminate(Matrix *mat, Matrix *b)
 {
     int n = mat->r;
-    for (int k = 0; k < n - 1; k++) {
-        if (mat->data[k][k] == 0) {
+    for (int k = 0; k < n - 1; k++)
+    {
+        int iMaxAbs = k;
+        int maxAbs = fabs(mat->data[k][k]);
+        for (int i = k + 1; i < n; i++)
+        {
+            int curAbs = fabs(mat->data[i][k]);
+            if (curAbs > maxAbs)
+            {
+                maxAbs = curAbs;
+                iMaxAbs = i;
+            }
+        }
+
+        if (iMaxAbs != k)
+        {
+            double *temp = mat->data[k];
+            mat->data[k] = mat->data[iMaxAbs];
+            mat->data[iMaxAbs] = temp;
+        }
+
+        if (mat->data[k][k] == 0)
+        {
             return 1;
         }
-        for (int i = k + 1; i < n; i++) {
+        for (int i = k + 1; i < n; i++)
+        {
             double factor = mat->data[i][k] / mat->data[k][k];
-            for (int j = k; j < mat->c; j++) {
+            for (int j = k; j < mat->c; j++)
+            {
                 mat->data[i][j] -= factor * mat->data[k][j];
             }
             b->data[i][0] -= factor * b->data[k][0];
